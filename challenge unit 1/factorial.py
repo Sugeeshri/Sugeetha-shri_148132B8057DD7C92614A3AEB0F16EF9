0d616d634e3719607c3d64b8602bc1ac747446cd
Sugeeshri <144246128+Sugeeshri@users.noreply.github.com>
2023-09-06 19:31:48 +0530

    Add files via upload

---
+++ challenge unit 1/factorial.py
@@ -0,0 +1,10 @@
+#Finding Factorial
+def factorial(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*factorial(n-1)
+number=5
+result=factorial(number)
+print("The factorial of {} is {}".format(number,result))
+    
---
+++ challenge unit 1/leap year.py
@@ -0,0 +1,7 @@
+# To find the leap year
+y = 2005 
+if (y % 4 == 0):
+  print("{} is a leap year".format(y))
+else:
+  print("{} is not a leap year".format(y))
+  
